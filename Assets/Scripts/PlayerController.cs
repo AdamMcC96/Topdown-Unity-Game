b3b3dc0ae9c70b64aaaec5ff81f34f3d430bd1aa
AdamMcC96 <adammccarthy96@hotmail.com>
2023-05-31 00:35:14 +0100

    Added PlayerController script with topdown movement and dash. Added CameraController to follow the player.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,54 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float moveSpd;
+    public Rigidbody2D rb2d;
+    private Vector2 moveInput;
+
+    private float activeMoveSpd;
+    public float dashSpd;
+    public float dashLgth = .5f, dashCooldwn = 1f;
+
+    private float dashCounter;
+    private float dashCoolCounter;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        activeMoveSpd = moveSpd;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        moveInput.x = Input.GetAxisRaw("Horizontal");
+        moveInput.y = Input.GetAxisRaw("Vertical");
+
+        moveInput.Normalize();
+        rb2d.velocity = moveInput * activeMoveSpd;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (dashCoolCounter <= 0 && dashCounter <= 0)
+            {
+                activeMoveSpd = dashSpd;
+                dashCounter = dashLgth;
+            }
+        }
+
+        if (dashCounter > 0)
+        {
+            dashCounter -= Time.deltaTime;
+
+            if (dashCounter <= 0)
+            {
+                activeMoveSpd = moveSpd;
+                dashCoolCounter = dashCooldwn;
+            }
+        }
+        if (dashCoolCounter > 0)
+        {
+            dashCoolCounter -= Time.deltaTime;
+        }
     }
 }
